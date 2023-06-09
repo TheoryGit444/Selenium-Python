ffa56705b5155d21b58520cee375ed88c566672f
Priyanka <113256199+TheoryGit444@users.noreply.github.com>
2023-06-09 15:42:36 -0400

    Create filesearch.py

---
+++ filesearch.py
@@ -0,0 +1,33 @@
+
+file_reader=open("InfosyDOG.txt","r")
+
+list=file_reader.readlines()
+
+
+
+list1=[]
+
+search_string="infosys dog"
+
+#find string in file and spilt and save it in list.
+
+for x in list:
+    
+    y=x.lower().find(search_string)
+    
+    if(y>=0):
+        list1.append(x)
+    
+print(list1)   
+
+#copy the content of list in file
+file_writer=open("Finalfile.txt","a")
+
+for x in list1:
+    file_writer.write(x)
+
+file_reader.close()    
+
+file_writer.close()
+
+# Yipeeee I win
